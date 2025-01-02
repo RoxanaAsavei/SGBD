cae4c0bb83b4d103a9176cd3616e540df1a76bb0
RoxanaAsavei <146893046+RoxanaAsavei@users.noreply.github.com>
2025-01-02 03:16:36 +0200

    Create delete.sql

---
+++ Proiect/delete.sql
@@ -0,0 +1,23 @@
+---------------------------------------------
+-- secventa de stergere
+
+DROP TABLE medici_ras CASCADE CONSTRAINTS;
+DROP TABLE asistenti_medicali_ras CASCADE CONSTRAINTS;
+DROP TABLE personal_nemedical_ras CASCADE CONSTRAINTS;
+DROP TABLE personal_administrativ_ras CASCADE CONSTRAINTS;
+DROP TABLE angajati_vorbesc_limbi_ras CASCADE CONSTRAINTS;
+DROP TABLE pacienti_vorbesc_limbi_ras CASCADE CONSTRAINTS;
+DROP TABLE pacienti_aleg_analize_ras CASCADE CONSTRAINTS;
+DROP TABLE pacient_medic_proced_ras CASCADE CONSTRAINTS;
+DROP TABLE etaj_ras CASCADE CONSTRAINTS;
+DROP TABLE personal_resp_etaj_ras CASCADE CONSTRAINTS;
+DROP TABLE camere_ras CASCADE CONSTRAINTS;
+DROP TABLE istoric_medical_ras CASCADE CONSTRAINTS;
+DROP TABLE analize_ras CASCADE CONSTRAINTS;
+DROP TABLE calificare_ras CASCADE CONSTRAINTS;
+DROP TABLE limba_ras CASCADE CONSTRAINTS;
+DROP TABLE sectie_ras CASCADE CONSTRAINTS;
+DROP TABLE proceduri_ras CASCADE CONSTRAINTS;
+DROP TABLE pacienti_ras CASCADE CONSTRAINTS;
+DROP TABLE angajati_ras;
+--------------------------------------------------
